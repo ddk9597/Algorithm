effd7a97c97660f90471a29c05c8a02630dc7f50
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-06-30 23:34:20 +0900

    15. 등차수열, 등비수열인지 확인하는 방법

---
+++ demo/src/main/java/com/training/Solution015.java
@@ -0,0 +1,32 @@
+package com.training;
+
+/* 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요. 
+ * 
+ * 2 < common의 길이 < 1,000
+-1,000 < common의 원소 < 2,000
+common의 원소는 모두 정수입니다.
+등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+등비수열인 경우 공비는 0이 아닌 정수입니다.
+ * 
+ * */
+public class Solution015 {
+
+	public int solution(int[] common) {
+		int answer = 0;
+		int n = common.length;
+
+		// 등차수열 확인
+		if (common[1] - common[0] == common[2] - common[1]) {
+			int d = common[1] - common[0]; // 공차
+			answer = common[n - 1] + d;
+		}
+		// 등비수열 확인
+		else if (common[1] / common[0] == common[2] / common[1]) {
+			int r = common[1] / common[0]; // 공비
+			answer = common[n - 1] * r;
+		}
+
+		return answer;
+	}
+
+}
