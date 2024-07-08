6e45416e1bbc03ad7cd3afe19070d243673b3759
DESKTOP-QDV2AJ2\Galaxy Book Pro <027620@naver.com>
2024-07-08 20:56:34 +0900

    처음부터 다시 해보자..

--- realator/src/main/java/mainContorller/MainController.java
+++ realator/src/main/java/mainContorller/MainController.java
@@ -2,13 +2,23 @@ package mainContorller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import service.MainService;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
 	
-	@GetMapping
-	public String home() {
-		return "index";
+	private final MainService service;
+	
+	@PostMapping("")
+	public String main() {
+		return "searchresult/searchResult";
 	}
-
+	
 }
